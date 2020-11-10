a03030de89279c92ef13058d3ace0c04df37efb9
hugopichard <43885826+hugopichard@users.noreply.github.com>
2020-11-10 09:33:20 +0100

    save 10/11/20

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function test_code() {
   let r = window.prompt("Code secret?");
       if (r == 2658) {
-          document.location.href="https://www.youtube.com/watch?v=_JW5gh-VbvI&ab_channel=lioneljeff63";
+          document.location.href="https://www.youtube.com/watch?v=_JW5gh-VbvI&ab_channel=lioneljeff63"; 
       }
       else {
           alert("Faux");
